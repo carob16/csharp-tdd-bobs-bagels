9ca0452bad7bbbc464c11ff1d52ce3ce1572076a
Caroline Berg <caroline.berg@no.experis.com>
2024-01-15 21:36:00 +0100

    Tests for user story 1 and 2 added

--- csharp-tdd-bobs-bagels.tests/UnitTest1.cs
+++ csharp-tdd-bobs-bagels.tests/UnitTest1.cs
@@ -1,5 +1,9 @@
+using System.Threading.Tasks.Sources;
+using tdd_bobs_bagels.CSharp.Main;
+
 namespace csharp_tdd_bobs_bagels.tests;
 
+[TestFixture]
 public class Tests
 {
     [SetUp]
@@ -7,9 +11,35 @@ public class Tests
     {
     }
 
-    [Test]
-    public void Test1()
+    [TestCase("BLT")]
+    [TestCase("Cream cheese")]
+    [TestCase("Dark chocolate")]
+    public void addBagel(string bagel)
     {
-        Assert.Pass();
+        Core basket = new Core();
+
+        string[] Result = basket.addBagel(bagel);
+
+        Assert.IsNotEmpty(Result);                
+        Assert.That(bagel == Result[Result.Length-1]);
+        Assert.That(Result.Contains(bagel));
+    }
+
+    [TestCase("BLT")]
+    [TestCase("Cream cheese")]
+    [TestCase("White chocolate")]//Does not exist in Basket
+    public void removeBagel(string bagelToRemove)
+    {
+        Core basket = new Core();
+        string[] bagelsToAdd = { "BLT", "Ham and cheese", "Cream cheese", "Chocolate" };
+        foreach(string bagel in bagelsToAdd)
+        {
+            basket.addBagel(bagel); 
+        }
+
+        string[] Result = basket.removeBagel(bagelToRemove);
+
+        Assert.IsTrue(Result.Length == bagelsToAdd.Length-1);
+        Assert.IsFalse(Result.Contains(bagelToRemove));
     }
 }
--- tdd-bobs-bagels.CSharp.Main/Core.cs
+++ tdd-bobs-bagels.CSharp.Main/Core.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace tdd_bobs_bagels.CSharp.Main
 {
+    
     public class Core
     {
+
+        public void Basket() { 
+                
+        }
+        
+        public string[] addBagel(string bagel) {
+            throw new NotImplementedException();
+        }
+
+        public bool removeBagel() { throw new NotImplementedException(); }
+
+        public string[] removeBagel(string bagel)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
